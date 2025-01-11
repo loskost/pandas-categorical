a44c7f495f08ce4b2f6818c4dbc3c4f7a1f4ee5f
loskost <loskost@ya.ru>
2025-01-11 20:26:16 +0300

    build: update version to 1.2.0

--- pandas_categorical/__init__.py
+++ pandas_categorical/__init__.py
@@ -1,1 +1,8 @@
-from pandas_categorical.utils import merge_categorical, concat_categorical, cat_astype
+from pandas_categorical.utils import (
+    merge_categorical,  # noqa: F401
+    concat_categorical,  # noqa: F401
+    cat_astype,  # noqa: F401
+)
+
+
+__version__ = "1.2.0"
